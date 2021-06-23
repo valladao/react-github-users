9f0f45c7295b06e90497639a833424ed068104e0
valladao <valladao@gmail.com>
2021-06-22 23:29:56 -0300

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+        Search a Github User
+      </h1>
     </div>
   );
 }
