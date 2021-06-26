f3ff38cc342ceb6f1903d1ff9a7074231ecedd6a
valladao <valladao@gmail.com>
2021-06-25 23:16:51 -0300

    Store the username in the state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,13 @@
-import React from "react"
-
-const handleSubmit = (event) => {
-  event.preventDefault()
-  alert("test")
-}
+import React, { useState } from "react"
 
 const Form = (props) => {
+  const [username, setUsername] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(username)
+  }
+
   return (
     <form className="w-full max-w-sm mx-auto" onSubmit={handleSubmit}>
       <div className="mt-10 flex">
@@ -13,6 +15,8 @@ const Form = (props) => {
           type="text"
           placeholder="GitHub username"
           className="form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
           required
         />
         <button
